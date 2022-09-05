81d7a1036e1aca8f7a394ec0cdf627460e6c61de
Bellier-Oeba <75356346+Bellier-Oeba@users.noreply.github.com>
2022-09-05 09:51:32 +0200

    Test 'card-back-section'

--- js/client.js
+++ js/client.js
@@ -1,30 +1,19 @@
-var Promise = TrelloPowerUp.Promise;
-
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
 window.TrelloPowerUp.initialize({
-  'attachment-sections': function (t, options) {
-    // we will just claim urls for Yellowstone
-    var claimed = options.entries.filter(function (attachment) {
-      return attachment.url.indexOf('http://www.nps.gov/yell/') === 0;
-    });
-
-    if (claimed && claimed.length > 0) {
-      return [{
-        id: 'Yellowstone', // optional if you aren't using a function for the title
-        claimed: claimed,
-        icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
-        title: 'Example Attachment Section: Yellowstone',
-        content: {
-          type: 'iframe',
-          url: t.signUrl('./section.html', {
-            arg: 'you can pass your section args here'
-          }),
-          height: 230
-        }
-      }];
-    } else {
-      return [];
-    }
+  'card-back-section': function (t, options) {
+    return {
+      title: 'My Card Back Section',
+      icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
+      content: {
+        type: 'iframe',
+        url: t.signUrl('./section.html'),
+        height: 230, // Max height is 1500.
+      },
+      action: {
+        text: 'My Action',
+        callback: (t) => console.log(t),
+      }
+    };
   }
 });
