773a5dce95fce300d5896e8e0926eff4dfda6342
Bellier-Oeba <75356346+Bellier-Oeba@users.noreply.github.com>
2022-09-05 12:45:09 +0200

    Save dates of card

--- js/section.js
+++ js/section.js
@@ -1,9 +1,37 @@
-let prod = document.getElementById('prod');
-let command = document.getElementById('command');
+const trello = window.TrelloPowerUp.iframe();
+
+const prod = document.getElementById('prod');
+const command = document.getElementById('command');
+const install = document.getElementById('install');
 
 prod.addEventListener('change', () => {
   let week = Number(prod.value.slice(-2)) - 2;
   let year = prod.value.slice(0, 4);
 
   command.value = year + '-W' + week;
+
+  trello.set('card', 'shared', 'prod-date', prod.value);
+  trello.set('card', 'shared', 'command-date', command.value);
 });
+
+install.addEventListener('change', () => {
+  trello.set('card', 'shared', 'install-date', install.value);
+});
+
+trello.get('card', 'shared', 'command-date')
+  .then((data) => {
+    console.log(JSON.stringify(data, null, 2));
+    command.value = data;
+  });
+
+trello.get('card', 'shared', 'prod-date')
+  .then((data) => {
+    console.log(JSON.stringify(data, null, 2));
+    prod.value = data;
+  });
+
+trello.get('card', 'shared', 'install-date')
+  .then((data) => {
+    console.log(JSON.stringify(data, null, 2));
+    install.value = data;
+  });
