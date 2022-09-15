a4836ed43bcfc1ed158d822dc5596be4237d7be7
Bellier-Oeba <75356346+Bellier-Oeba@users.noreply.github.com>
2022-09-15 12:23:16 +0200

    Fix calculation of command week when prod week is the first or seconde week of year

--- js/section.js
+++ js/section.js
@@ -4,9 +4,26 @@ const prod = document.getElementById('prod');
 const command = document.getElementById('command');
 const install = document.getElementById('install');
 
+const getISOWeeks = (y) => {
+  let d = new Date(y, 0, 1);
+  let isLeap = new Date(y, 1, 29).getMonth() === 1;
+  return d.getDay() === 4 || isLeap && d.getDay() === 3 ? 53 : 52
+}
+
 prod.addEventListener('change', () => {
-  let week = Number(prod.value.slice(-2)) - 2;
+  let week = Number(prod.value.slice(-2));
   let year = prod.value.slice(0, 4);
+
+  if(week === 1) {
+    week = ('00' + getISOWeeks(year)).slice(-2) - 2
+    year = year - 1
+  } else if (week === 2) {
+    week = ('00' + getISOWeeks(year)).slice(-2) - 1
+    year = year - 1
+  } else {
+    week = ('00' + (week - 2)).slice(-2)
+    console.log(week)
+  }
 
   command.value = year + '-W' + week;
 
