056f91d141275b0a422baeb6e3edc3d592bf9838
Bellier-Oeba <75356346+Bellier-Oeba@users.noreply.github.com>
2024-12-19 08:52:13 +0100

    V2.0.0: Complete rework of card section
    - Switch to Flatpickr lib for pickers
    - Remove unused install picker
    - Embed js libraries to fix version

--- js/section.js
+++ js/section.js
@@ -1,53 +1,46 @@
 const trello = window.TrelloPowerUp.iframe();
 
-const prod = document.getElementById('prod');
-const command = document.getElementById('command');
-const install = document.getElementById('install');
+const daysToSubtract = 14;
 
-const weekToSubtract = 2;
+const pickerConfig = {
+	weekNumbers: true,
+	altInput: true,
+	altFormat: "\\Se\\ma\\i\\ne W, Y",
+	locale: fr.French,
+	plugins: [new weekSelect({})],
+};
 
-const subtractWeeks = (date) => {
-  const week = Number(date.slice(-2));
-  const year = Number(date.slice(0, 4));
-  
-  let newDate = new Date(year, 0).getTime() + week * 7 * 24 * 60 * 60 * 1000
-  newDate = newDate - (weekToSubtract * 604800000)
-  
-  const now = new Date(newDate);
-  const onejan = new Date(now.getFullYear(), 0, 1);
-  let weekNumber = Math.ceil((((now.getTime() - onejan.getTime()) / 86400000) + onejan.getDay() + 1) / 7) - 1;
-
-  if (weekNumber === 0) weekNumber = 1
-
-  return now.getFullYear() + '-W' + ('00' + weekNumber).slice(-2);
-}
-
-prod.addEventListener('change', () => {
-  command.value = prod.value !== '' ? subtractWeeks(prod.value) : '';
-
-  trello.set('card', 'shared', 'prod-date', prod.value);
-  trello.set('card', 'shared', 'command-date', command.value);
+const command = flatpickr("#command", {
+	...pickerConfig,
+	onChange: [
+		(_, date) => {
+			trello.set("card", "shared", "command-date", date);
+		},
+	],
 });
 
-command.addEventListener('change', () => {
-  trello.set('card', 'shared', 'command-date', command.value);
+const prod = flatpickr("#prod", {
+	...pickerConfig,
+	onChange: [
+		(_, date) => {
+			let commandDate = new Date(date);
+			commandDate.setDate(commandDate.getDate() - daysToSubtract);
+			commandDate = commandDate.toISOString().split("T")[0];
+
+			// Update the command date in command picker
+			command.setDate(commandDate);
+
+			// Save new dates in the database
+			trello.set("card", "shared", "prod-date", date);
+			trello.set("card", "shared", "command-date", commandDate);
+		},
+	],
 });
 
-install.addEventListener('change', () => {
-  trello.set('card', 'shared', 'install-date', install.value);
+trello.get("card", "shared", "command-date").then((data) => {
+	command.setDate(data);
 });
 
-trello.get('card', 'shared', 'command-date')
-  .then((data) => {
-    command.value = data;
-  });
-
-trello.get('card', 'shared', 'prod-date')
-  .then((data) => {
-    prod.value = data;
-  });
-
-trello.get('card', 'shared', 'install-date')
-  .then((data) => {
-    install.value = data;
-  });
+trello.get("card", "shared", "prod-date").then((data) => {
+	prod.setDate(data);
+});
